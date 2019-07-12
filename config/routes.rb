c266bdc5635f1535e45a494bcef8735e615711f9
Nathan L. Walls <nathan@wallscorp.us>
2019-07-11 22:00:09 -0400

    Add in the web UI for Sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
+  require 'sidekiq/web'
+
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
 
   get "/sign_up" => "users#new"
   get "/sign_in" => "sessions#new"
   get "/sign_out" => "sessions#destroy"
+
+  namespace :admin do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 
   #root "/pages/home.html"
 end
