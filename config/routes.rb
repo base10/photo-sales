6947d2c02505f1b75ce9850a2fb22d339e7cbe56
Nathan L. Walls <nathan@wallscorp.us>
2019-07-11 22:42:58 -0400

    Initialize Administrate with a User dashboard and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  namespace :admin do
+      resources :users
+
+      root to: "users#index"
+    end
   require 'sidekiq/web'
 
   resource :session, only: [:new, :create, :destroy]
