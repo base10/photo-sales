76ad2ac2e30438d59ba98136401ca4b48f8d66e2
Nathan L. Walls <nathan@wallscorp.us>
2021-01-17 23:23:56 -0500

    Upgrade to Rails 6.1

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,28 @@
 require_relative "boot"
+
 require "rails/all"
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+
 module PhotoSales
   class Application < Rails::Application
+    # Initialize configuration defaults for originally generated Rails version.
+    config.load_defaults 5.0
+
+    # Configuration for the application, engines, and railties goes here.
+    #
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
+    #
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
+
+    config.action_controller.action_on_unpermitted_parameters = :raise
+    config.active_job.queue_adapter = :sidekiq
     config.assets.quiet = true
+
     config.generators do |generate|
       generate.helper false
       generate.javascript_engine false
@@ -13,8 +32,5 @@ module PhotoSales
       generate.test_framework :rspec
       generate.view_specs false
     end
-
-    config.action_controller.action_on_unpermitted_parameters = :raise
-    config.active_job.queue_adapter = :sidekiq
   end
 end
